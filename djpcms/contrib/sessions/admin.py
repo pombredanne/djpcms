aa437b9cd9161cc3c561cc60e9283559e4721522
quantmind <luca.sbardella@citi.com>
2011-07-15 17:24:01 +0100

    minor in sessions admin

--- djpcms/contrib/sessions/admin.py
+++ djpcms/contrib/sessions/admin.py
@@ -13,30 +13,34 @@ class UserAdmin(TabViewMixin,UserApplicationWithFilter):
 
 
 admin_urls = (
-              UserAdmin('/users/',
-                        User,
-                        name = 'Users',
-                        list_display = ('username','first_name','last_name',
-                                        'email','is_active','is_superuser')),
-              AdminApplication('/groups/',
-                               Group,
-                               form = GroupForm,
-                               list_display = ('name','description')),
-              AdminApplication('/roles/',
-                               Role,
-                               form = RoleForm,
-                               name = 'Roles',
-                               list_display = ['action','numeric_code','model_type','object']),
-              AdminApplication('/permissions/',
-                               ObjectPermission,
-                               name = 'Object Permissions',
-                               list_display = ['role','user','group','action']),
-              AdminApplicationSimple('/logs/',
-                                     Log,
-                                     name='Logs',
-                                     list_display = ('timestamp','level','source','msg','host'),
-                                     object_display = ('timestamp','level','source',
-                                                       'host','msg')
-                                     )
-            )
+    UserAdmin('/users/',
+              User,
+              name = 'Users',
+              list_display = ('username','first_name','last_name',
+                              'email','is_active','is_superuser')),
+    AdminApplication('/groups/',
+                     Group,
+                     form = GroupForm,
+                     list_display = ('name','description')),
+    AdminApplication('/roles/',
+                     Role,
+                     form = RoleForm,
+                     name = 'Roles',
+                     list_display = ('action','numeric_code',
+                                     'model_type','object')),
+    AdminApplication(
+             '/permissions/',
+             ObjectPermission,
+             name = 'Object Permissions',
+             list_display = ['role','user','group','action']),
+    AdminApplicationSimple(
+               '/logs/',
+               Log,
+               name='Logs',
+               list_display = ('timestamp','level','source',
+                               'msg','host','user'),
+               object_display = ('timestamp','level','source',
+                                 'host','msg')
+               )
+)
 

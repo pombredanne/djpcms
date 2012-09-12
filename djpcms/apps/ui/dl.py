aa5d872f046069a045a69f670c9031573b1a32c4
quantmind <luca.sbardella@citi.com>
2012-09-12 17:01:27 +0100

    css dl fix

--- djpcms/apps/ui/dl.py
+++ djpcms/apps/ui/dl.py
@@ -5,6 +5,18 @@ cssv.definition_list.opacity = 0.8
 cssv.definition_list.font_weight = 'normal'
 
 ################################################# OBJECT DEFINITIONS
+class dl(mixin):
+    
+    def __init__(self, width):
+        self.width = pc(width)
+        
+    def __call__(self, elem):
+        css('dl',
+            css('dt', width=self.width),
+            css('dd', margin_left=self.width),
+            parent=elem)
+        
+        
 css('.%s' % classes.object_definition,
     css('dl',
         cssa(':first-child', margin_top=0),
@@ -12,15 +24,18 @@ css('.%s' % classes.object_definition,
             opacity(cssv.definition_list.opacity),
             font_weight=cssv.definition_list.font_weight,
             float='left',
-            width='40%',
             margin=0),
-        css('dd', margin = 0),
-        cssa('.w20 dt', width='20%'),
-        cssa('.w40 dt', width='40%'),
-        cssa('.w50 dt', width='50%'),
+        css('dd', margin=0),
         float='left',
         width=pc(100),
         margin= spacing(cssv.definition_list.spacing, 0, 0)),
     float='left',
     width=pc(100),
     comment='Object definition list')
+
+
+css('.%s' % classes.object_definition,
+    dl(40),
+    cssa('.w20', dl(20)),
+    cssa('.w30', dl(30)),
+    cssa('.w50', dl(50)))
